6dd7a8924e9bca327d2fb1c6907439c24ce85c26
polaris65b <42924432+polaris65b@users.noreply.github.com>
2025-02-14 22:10:08 +0900

    Feat:add CommentRepository

--- src/main/java/com/example/nbcnewsfeed/comment/repository/CommentRepository.java
+++ src/main/java/com/example/nbcnewsfeed/comment/repository/CommentRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public class CommentRepository extends JpaRepository<Comment, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByPostId(Long postId);
 }
