a01435265e010bc87f69d11caaec91b41bd6dfe8
polaris65b <42924432+polaris65b@users.noreply.github.com>
2025-02-14 20:16:25 +0900

    first commit

---
+++ src/main/java/com/example/nbcnewsfeed/NbcNewsFeedApplication.java
@@ -0,0 +1,13 @@
+package com.example.nbcnewsfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcNewsFeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbcNewsFeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nbcnewsfeed/NbcNewsFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nbcnewsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbcNewsFeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
