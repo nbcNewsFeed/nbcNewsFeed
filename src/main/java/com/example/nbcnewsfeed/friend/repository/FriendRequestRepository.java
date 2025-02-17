ec11b5c5bcab4bdeb69eb9c8a0e2d27c3cfe3884
minjonyyy <minjonyyy@naver.com>
2025-02-17 11:21:59 +0900

    feat : 친구 요청 및 친구 상태를 위한 테이블 분리

---
+++ src/main/java/com/example/nbcnewsfeed/friend/repository/FriendRequestRepository.java
@@ -0,0 +1,14 @@
+package com.example.nbcnewsfeed.friend.repository;
+
+import com.example.nbcnewsfeed.friend.entity.FriendRequest;
+import com.example.nbcnewsfeed.friend.entity.FriendStatus;
+import com.example.nbcnewsfeed.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FriendRequestRepository extends JpaRepository<FriendRequest, Long> {
+
+    boolean existsFriendRequestBySenderIdAndReceiverIdAndFriendStatus(Long senderId, Long receiverId, FriendStatus friendStatus);
+
+}
