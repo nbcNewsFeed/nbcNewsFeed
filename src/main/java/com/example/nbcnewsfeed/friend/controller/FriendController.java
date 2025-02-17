fea6596c63c737a1f9491869b8c43ae4957632a8
minjonyyy <minjonyyy@naver.com>
2025-02-17 11:19:03 +0900

    refactor : 친구 요청 dto로 전달

--- src/main/java/com/example/nbcnewsfeed/friend/controller/FriendController.java
+++ src/main/java/com/example/nbcnewsfeed/friend/controller/FriendController.java
@@ -1,5 +1,6 @@
 package com.example.nbcnewsfeed.friend.controller;
 
+import com.example.nbcnewsfeed.friend.dto.CreateFriendRequestDto;
 import com.example.nbcnewsfeed.friend.service.FriendService;
 import com.example.nbcnewsfeed.user.UserClient;
 import lombok.RequiredArgsConstructor;
@@ -16,23 +17,18 @@ public class FriendController {
     private final FriendService friendService;
 
     // 친구 요청 전송 컨트롤러
-    @PostMapping("/{receiverId}")
+    @PostMapping
     public ResponseEntity<String> sendFriendRequest (
-            @PathVariable Long receiverId,
-            @RequestParam Long senderId){ //JWT 로그인 구현 이후에는 @RequestHeader("Authorization") String token 으로 변경
+            @RequestBody CreateFriendRequestDto requestDto){ //JWT 로그인 구현 이후에는 @RequestHeader("Authorization") String token 으로 변경
 
         //JWT에서 사용자 Id 추출
 //        Long senderId = extractUserIdFromToken(token);
 
-        if (senderId == null){
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "senderId is null");
-        }
-
-        if (senderId.equals(receiverId)){ //JWT 로그인 구현 이후에 변경
+        if (requestDto.getSenderId().equals(requestDto.getReceiverId())){ //JWT 로그인 구현 이후에 변경
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "자기 자신에게는 친구 요청이 불가능합니다. ");
         }
 
-        friendService.sendFriendRequest(senderId, receiverId);
+        friendService.sendFriendRequest(requestDto);
 
         return new ResponseEntity<>("친구 요청이 전송되었습니다.", HttpStatus.OK);
 
