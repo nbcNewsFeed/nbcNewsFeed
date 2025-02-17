da70c3b1999f628460ca2d6e2e7909788179cf2b
minjonyyy <minjonyyy@naver.com>
2025-02-17 13:35:24 +0900

    chore : 엔티티 항목 추가

--- src/main/java/com/example/nbcnewsfeed/user/User.java
+++ src/main/java/com/example/nbcnewsfeed/user/User.java
@@ -2,30 +2,55 @@ package com.example.nbcnewsfeed.user;
 
 import com.example.nbcnewsfeed.BaseEntity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
+import org.hibernate.validator.constraints.URL;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Table(name="users")
 public class User extends BaseEntity {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String username;
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
+    @NotBlank(message = "닉네임은 필수 입력 값입니다.")
+    @Column(nullable = false)
+    private String nickname;
 
-    @Column(unique = true, nullable = false)
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식이 올바르지 않습니다.")
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Column(nullable = false, unique = true)
     private String email;
 
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대소문자, 숫자, 특수문자를 사용하세요.")
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     @Column(nullable = false)
     private String password;
 
-    public User(String username, String password, String email) {
-        this.username = username;
-        this.password = password;
-        this.email = email;
-    }
+    @URL(message = "프로필 이미지 URL 형식이 아닙니다.")
+    @Column(name = "profile_image_url")
+    private String profileImageUrl;
 
+    @Column(name = "status_message")
+    private String statusMessage;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    public User(String nickname, String email, String password, String profileImageUrl, LocalDateTime deletedAt, String statusMessage) {
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+        this.profileImageUrl = profileImageUrl;
+        this.deletedAt = deletedAt;
+        this.statusMessage = statusMessage;
+    }
 
     public User() {
 
