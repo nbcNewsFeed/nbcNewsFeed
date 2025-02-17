c91962f7adbbae7a84bcac6d86ead3b477fea8a4
Seoyeon <dev_seoyeon@mythologyteams.com>
2025-02-17 16:57:24 +0900

    Refactor: dto 및 setter 수정

--- src/main/java/com/example/nbcnewsfeed/user/dto/ChangePasswordDto.java
+++ src/main/java/com/example/nbcnewsfeed/user/dto/ChangePasswordDto.java
@@ -1,14 +1,20 @@
 package com.example.nbcnewsfeed.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class ChangePasswordDto {
-    private final String currentPassword;
+
+    private final String inputPassword;
+
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대소문자, 숫자, 특수문자를 사용하세요.")
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     private final String newPassword;
 
-    public ChangePasswordDto(String currentPassword, String newPassword) {
-        this.currentPassword = currentPassword;
+    public ChangePasswordDto(String inputPassword, String newPassword) {
+        this.inputPassword = inputPassword;
         this.newPassword = newPassword;
     }
 }
--- src/main/java/com/example/nbcnewsfeed/user/entity/User.java
+++ src/main/java/com/example/nbcnewsfeed/user/entity/User.java
@@ -2,13 +2,10 @@ package com.example.nbcnewsfeed.user.entity;
 
 import com.example.nbcnewsfeed.common.entity.BaseEntity;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.Filter;
 import org.hibernate.annotations.FilterDef;
-import org.hibernate.validator.constraints.URL;
 
 import java.time.LocalDateTime;
 
@@ -25,22 +22,15 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
-    @NotBlank(message = "닉네임은 필수 입력 값입니다.")
     @Column(nullable = false)
     private String nickname;
 
-    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식이 올바르지 않습니다.")
-    @NotBlank(message = "이메일은 필수 입력 값입니다.")
     @Column(nullable = false, unique = true)
     private String email;
 
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대소문자, 숫자, 특수문자를 사용하세요.")
-    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     @Column(nullable = false)
     private String password;
 
-    @URL(message = "프로필 이미지 URL 형식이 아닙니다.")
     @Column(name = "profile_image_url")
     private String profileImageUrl;
 
@@ -62,4 +52,18 @@ public class User extends BaseEntity {
     public User() {
 
     }
+
+    // 수정 메소드
+    public void changeProfileImageUrl(String newProfileImageUrl) {
+        this.profileImageUrl = newProfileImageUrl;
+    }
+
+    public void changeStatusMessage(String newStatusMessage) {
+        this.statusMessage = newStatusMessage;
+    }
+
+    public void changeNickname(String newNickname) {
+        this.nickname = newNickname;
+    }
+
 }
