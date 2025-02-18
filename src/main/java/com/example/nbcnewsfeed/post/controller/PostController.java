85925a3dfe7717e756c98220858e1521d672f810
codingTrip <codingtrip@naver.com>
2025-02-18 14:46:18 +0900

    Refactor: 주석 부분 제거

--- src/main/java/com/example/nbcnewsfeed/post/controller/PostController.java
+++ src/main/java/com/example/nbcnewsfeed/post/controller/PostController.java
@@ -1,93 +1,93 @@
-//package com.example.nbcnewsfeed.post.controller;
-//
-//import com.example.nbcnewsfeed.post.dto.request.PostSaveRequestDto;
-//import com.example.nbcnewsfeed.post.dto.request.PostUpdateRequestDto;
-//import com.example.nbcnewsfeed.post.dto.response.PostPageResponseDto;
-//import com.example.nbcnewsfeed.post.dto.response.PostResponseDto;
-//import com.example.nbcnewsfeed.post.dto.response.PostSaveResponseDto;
-//import com.example.nbcnewsfeed.post.dto.response.PostUpdateResponseDto;
-//import com.example.nbcnewsfeed.post.service.PostService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.data.domain.Page;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.validation.annotation.Validated;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequiredArgsConstructor
-//public class PostController {
-//
-//    private final PostService postService;
-//
-//    //게시글 등록(C)
-//    @PostMapping("/posts")
-//    public ResponseEntity<PostSaveResponseDto> save(
-//            //토큰 받아 올 자리
-////            @SessionAttribute(name = Const.LOGIN_USER) Long userId;
-//            @Validated @RequestBody PostSaveRequestDto requestDto
-//    ) {
-//        return new ResponseEntity<>(postService.save(
-////                userId,
-//                requestDto), HttpStatus.CREATED);
-//    }
-//
-//    //게시글 다건 조회(R) 페이지네이션
-//    @GetMapping("/posts")
-//    public ResponseEntity<Page<PostPageResponseDto>> findAllPage(
-//            @RequestParam(defaultValue = "1") int page,
-//            @RequestParam(defaultValue = "10") int size
-//    ) {
-//        Page<PostPageResponseDto> result = postService.findAllPage(page, size);
-//        return ResponseEntity.ok(result);
-//    }
-//
-//    //게시글 단건 조회(R)
-//    @GetMapping("/posts/{postId}")
-//    public ResponseEntity<PostResponseDto> findById(
-//            @PathVariable Long postId
-//    ) {
-//        return ResponseEntity.ok(postService.findById(postId));
-//    }
-//
-//
-//    //게시글 수정(U)
-//    @PutMapping("/posts/{postId}")
-//    public ResponseEntity<PostUpdateResponseDto> update(
-//            @PathVariable Long postId,
-//            //토큰 받아 올 자리
-////            @SessionAttribute(name = Const.LOGIN_USER) Long userId;
-//            @Validated @RequestBody PostUpdateRequestDto requestDto
-//    ) {
-//        return ResponseEntity.ok(postService.update(
-//                postId,
-////                userId,
-//                requestDto
-//        ));
-//    }
-//
-//    //게시글 삭제(D)
-//    @DeleteMapping("/posts/{postId}")
-//    public ResponseEntity<Void> deleteById(
-//            @PathVariable Long postId
-//            //토큰 받아 올 자리
-////            , @SessionAttribute(name = Const.LOGIN_USER) Long userId;
-//
-//    ) {
-//        postService.deleteById(
-//                postId
-////                , userId
-//        );
-//        return ResponseEntity.ok().build();
-//    }
-//
-//    //삭제된 게시글 복구(삭제된 후 2주 지나기 전)
-//    @PutMapping("/posts/{postId}")
-//    public ResponseEntity<PostResponseDto> restore(
-//            @PathVariable Long postId
-//            //토큰 받아 올 자리
-////            , @SessionAttribute(name = Const.LOGIN_USER) Long userId;
-//    ) {
-//        return ResponseEntity.ok(postService.restore(postId));
-//    }
-//}
+package com.example.nbcnewsfeed.post.controller;
+
+import com.example.nbcnewsfeed.post.dto.request.PostSaveRequestDto;
+import com.example.nbcnewsfeed.post.dto.request.PostUpdateRequestDto;
+import com.example.nbcnewsfeed.post.dto.response.PostPageResponseDto;
+import com.example.nbcnewsfeed.post.dto.response.PostResponseDto;
+import com.example.nbcnewsfeed.post.dto.response.PostSaveResponseDto;
+import com.example.nbcnewsfeed.post.dto.response.PostUpdateResponseDto;
+import com.example.nbcnewsfeed.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    //게시글 등록(C)
+    @PostMapping("/posts")
+    public ResponseEntity<PostSaveResponseDto> save(
+            //토큰 받아 올 자리
+//            @SessionAttribute(name = Const.LOGIN_USER) Long userId;
+            @Validated @RequestBody PostSaveRequestDto requestDto
+    ) {
+        return new ResponseEntity<>(postService.save(
+//                userId,
+                requestDto), HttpStatus.CREATED);
+    }
+
+    //게시글 다건 조회(R) 페이지네이션
+    @GetMapping("/posts")
+    public ResponseEntity<Page<PostPageResponseDto>> findAllPage(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        Page<PostPageResponseDto> result = postService.findAllPage(page, size);
+        return ResponseEntity.ok(result);
+    }
+
+    //게시글 단건 조회(R)
+    @GetMapping("/posts/{postId}")
+    public ResponseEntity<PostResponseDto> findById(
+            @PathVariable Long postId
+    ) {
+        return ResponseEntity.ok(postService.findById(postId));
+    }
+
+
+    //게시글 수정(U)
+    @PutMapping("/posts/{postId}")
+    public ResponseEntity<PostUpdateResponseDto> update(
+            @PathVariable Long postId,
+            //토큰 받아 올 자리
+//            @SessionAttribute(name = Const.LOGIN_USER) Long userId;
+            @Validated @RequestBody PostUpdateRequestDto requestDto
+    ) {
+        return ResponseEntity.ok(postService.update(
+                postId,
+//                userId,
+                requestDto
+        ));
+    }
+
+    //게시글 삭제(D)
+    @DeleteMapping("/posts/{postId}")
+    public ResponseEntity<Void> deleteById(
+            @PathVariable Long postId
+            //토큰 받아 올 자리
+//            , @SessionAttribute(name = Const.LOGIN_USER) Long userId;
+
+    ) {
+        postService.deleteById(
+                postId
+//                , userId
+        );
+        return ResponseEntity.ok().build();
+    }
+
+    //삭제된 게시글 복구(삭제된 후 2주 지나기 전)
+    @PutMapping("/posts/{postId}")
+    public ResponseEntity<PostResponseDto> restore(
+            @PathVariable Long postId
+            //토큰 받아 올 자리
+//            , @SessionAttribute(name = Const.LOGIN_USER) Long userId;
+    ) {
+        return ResponseEntity.ok(postService.restore(postId));
+    }
+}
