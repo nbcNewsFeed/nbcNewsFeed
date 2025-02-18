4077b8b7c261d791b86204ca8735385001be149b
polaris65b <42924432+polaris65b@users.noreply.github.com>
2025-02-18 11:18:11 +0900

    feat: 댓글 엔티티에 activeUserFilter 추가 및 필드 제약 강화

--- src/main/java/com/example/nbcnewsfeed/comment/entity/Comment.java
+++ src/main/java/com/example/nbcnewsfeed/comment/entity/Comment.java
@@ -1,26 +1,34 @@
 package com.example.nbcnewsfeed.comment.entity;
 
+import com.example.nbcnewsfeed.common.entity.BaseEntity;
+import com.example.nbcnewsfeed.post.entity.Post;
+import com.example.nbcnewsfeed.user.entity.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Filter;
+import org.hibernate.annotations.FilterDef;
 
 @Getter
 @Entity
 @NoArgsConstructor
+@Filter(name = "activeUserFilter", condition = "user_id IN (SELECT id FROM user WHERE deleted_at IS NULL)")
+@FilterDef(name = "activeUserFilter")
 public class Comment extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(nullable = false)
     private String content;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "post_id")
+    @JoinColumn(name = "post_id", nullable = false)
     private Post post;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     public Comment(Post post, User user, String content){
